ccd37e09396132db4b008fd895a4f9721c936ab7
ttricktt <33535170+ttricktt@users.noreply.github.com>
2017-11-12 20:14:19 -0500

    Made changes to db_fns_php

--- upyoursbaby/db_fns.php
+++ upyoursbaby/db_fns.php
@@ -2,13 +2,13 @@
 
 //1. Create a database connection
 function db_connect() {
-$connection = mysql_pconnect("mysql","project","secret");
+$connection = mysqli_connect("mysql","project","secret");
 if (!$connection)  {
 	die("Database connection failed: " . mysql_error());
 	}
 
 // 2. Select a database to use
-$db_select = mysql_select_db("project");
+$db_select = mysqli_select_db($link, 'project');
 if (!$db_select)  {
 	die("Database selection failed: " . mysql_error());
 	}

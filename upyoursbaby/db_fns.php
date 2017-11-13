50701329313a37680e47d16623f139f669e11527
ttricktt <33535170+ttricktt@users.noreply.github.com>
2017-11-12 20:21:45 -0500

    mysqli_connect_error

--- upyoursbaby/db_fns.php
+++ upyoursbaby/db_fns.php
@@ -4,13 +4,13 @@
 function db_connect() {
 $connection = mysqli_connect("mysql","project","secret");
 if (!$connection)  {
-	die("Database connection failed: " . mysql_error());
+	die("Database connection failed: " . mysql_connect_error());
 	}
 
 // 2. Select a database to use
 $db_select = mysqli_select_db($link, 'project');
 if (!$db_select)  {
-	die("Database selection failed: " . mysql_error());
+	die("Database selection failed: " . mysql_connect_error());
 	}
 }
 	

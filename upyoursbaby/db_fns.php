0a521a8aad6e720c54ed186805f0b55ae227974f
ttricktt <33535170+ttricktt@users.noreply.github.com>
2017-11-12 20:24:25 -0500

    typo

--- upyoursbaby/db_fns.php
+++ upyoursbaby/db_fns.php
@@ -4,13 +4,13 @@
 function db_connect() {
 $connection = mysqli_connect("mysql","project","secret");
 if (!$connection)  {
-	die("Database connection failed: " . mysql_connect_error());
+	die("Database connection failed: " . mysqli_connect_error());
 	}
 
 // 2. Select a database to use
 $db_select = mysqli_select_db($link, 'project');
 if (!$db_select)  {
-	die("Database selection failed: " . mysql_connect_error());
+	die("Database selection failed: " . mysqi_connect_error());
 	}
 }
 	
